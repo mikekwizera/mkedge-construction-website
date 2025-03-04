cd66a40b8cbcb2a6e8023fa7d0f55d3967e6bc78
mikekwizera <mikekwizera4@gmail.com>
2025-03-04 03:37:50 +0200

    Implement Authentication backend

---
+++ backend/routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Http\Controllers\admin\HomeController;
+use App\Http\Controllers\AuthenticationController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::post('authenticate', [AuthenticationController::class, 'authenticate' ]);
+
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::group(['middleware' => ['auth:sanctun']], function(){
+    //Protected Routes
+    Route::get('home', [HomeController::class, 'index' ]);
+    Route::get('logout', [AuthenticationController::class, 'logout' ]);
+});
