4aa3aec7446a0cec4060d31139b667f4fe66bc94
mikekwizera <mikekwizera4@gmail.com>
2025-06-02 19:10:07 +0200

    Delete Article

--- backend/app/Http/Controllers/admin/ArticleController.php
+++ backend/app/Http/Controllers/admin/ArticleController.php
@@ -174,4 +174,29 @@ class ArticleController extends Controller
             'message' => 'Article updated successfully'
         ]);
     }
+
+       public function destroy($id) {
+
+        $article = Article::find($id);
+
+        if ($article == null){
+            return response()->json([
+                'status' => false,
+                'message' => 'Article not found'
+            ]); 
+        }
+
+        if ($article->image != '') {
+            File::delete(public_path('uploads/articles/large/'.$article->image));
+            File::delete(public_path('uploads/articles/small/'.$article->image));
+
+        }
+        
+        $article->delete();
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Article deleted successfully.'
+        ]);  
+    }
 }
