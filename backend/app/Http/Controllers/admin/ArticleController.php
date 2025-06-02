7842e435f2e18b19c3f9c25c375e75847ad42312
mikekwizera <mikekwizera4@gmail.com>
2025-06-02 19:04:25 +0200

    Update Article

--- backend/app/Http/Controllers/admin/ArticleController.php
+++ backend/app/Http/Controllers/admin/ArticleController.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Str;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 use App\Models\TempImage;
 use Intervention\Image\ImageManager;
 use Intervention\Image\Drivers\Gd\Driver;
@@ -99,5 +100,78 @@ class ArticleController extends Controller
                 'status' => true,
                 'message' => 'Article added successfully'
             ]);
+    }
+
+    public function update($id, Request $request) {
+
+        $article = Article::find($id);
+
+        if($article == null) {
+            return response()->json([
+                'status' => false,
+                'message' => "Article not found"
+            ]);
+        }
+
+        $request->merge(['slug' => Str::slug($request->slug)]);
+        
+        $validator = Validator::make($request->all(),[
+            'title' => 'required',
+            'slug' => 'required|unique:articles,slug'.$id.',id'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'errors' => $validator->errors()
+            ]);
+        }
+
+        $article->title = $request->title;
+        $article->slug =Str::slug($request->slug);
+        $article->author = $request->author;
+        $article->content = $request->content;
+        $article->status = $request->status;
+        $article->save();
+
+        //Save Temp image here 
+        if ($request->imageId > 0){
+            $oldImage = $article->image;
+            $tempImage = TempImage::find($request->imageId);
+            if ($tempImage != null){
+                $extArray = explode('.',$tempImage->name);
+                $ext = last($extArray);
+
+                $fileName = strtotime('now').$article->id.'.'.$ext;
+
+                //Create small thumbnail here
+                $sourcePath = public_path('uploads/temp/'.$tempImage->name);
+                $destPath = public_path('uploads/articles/small/'.$fileName);
+                $manager = new ImageManager(Driver::class);
+                $image = $manager->read($sourcePath );
+                $image->coverDown(450, 300);
+                $image->save($destPath);
+
+                //Create large thumbnail here
+                $destPath = public_path('uploads/articles/large/'.$fileName);
+                $manager = new ImageManager(Driver::class);
+                $image = $manager->read($sourcePath );
+                $image->scaleDown(1200);
+                $image->save($destPath);
+
+                $article->image = $fileName;
+                $article->save();
+
+                if($oldImage != ''){
+                    File::delete(public_path('uploads/articles/large/'.$oldImage));
+                    File::delete(public_path('uploads/articles/small/'.$oldImage));
+                }
+              }           
+            }
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Article updated successfully'
+        ]);
     }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -39,7 +39,8 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('articles', [ArticleController::class, 'store']);
     Route::get('articles', [ArticleController::class, 'index']);
     Route::get('articles/{id}', [ArticleController::class, 'show']);
-
+    Route::put('articles/{id}', [ArticleController::class, 'update']);
+    Route::delete('articles/{id}', [ArticleController::class, 'destroy']);
 
 
 
